c2d8df2a308db21d98a739dd53d1d1336d062bd0
tom-racine <tracine@la-joliverie.com>
2020-11-04 12:46:20 +0100

    Ajout de la fonctionnalité pour remplir la table lac (Commentaire dans MainActivity)

--- app/src/main/java/com/example/temperocalor/MainActivity.java
+++ app/src/main/java/com/example/temperocalor/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.temperocalor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,4 +46,23 @@ public class MainActivity extends AppCompatActivity {
         };
         listeMoyenne.setOnClickListener(ecouteur3);
     }
+
+    /*public void remplirTableLac() {
+        DAOBdd lacBdd = new DAOLac(this);
+        Lac lac1 = new Lac ( "L001", "Lac de Grand-Lieu", 47.095212, -1.673092);
+        Lac lac2 = new Lac ( "L002", "Lac de l'Espérance",46.779756,-0.853628 );
+        Lac lac3 = new Lac ( "L003", "Lac de la Vallée Mabille", 47.356276, -1.924115 );
+
+        //on ouvre la base de données
+        lacBdd.open();
+
+        //on insère chaque lac un par un
+        lacBdd.insererLac(lac1);
+        lacBdd.insererLac(lac2);
+        lacBdd.insererLac(lac3);
+
+        //le curseur pour afficher le nombre de lacs dans la base
+        Cursor c = lacBdd.getData();
+        Toast.makeText(getApplicationContext(), " il y a " + String.valueOf(c.getCount()) + " lacs ", Toast.LENGTH_LONG).show();
+    }*/
 }
